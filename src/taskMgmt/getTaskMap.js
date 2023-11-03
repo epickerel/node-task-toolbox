e95221badfad1a03b413e53342485e3b724bdcfb
Emmett Pickerel <emmett.pickerel@gmail.com>
2023-11-03 12:55:44 -0400

    remove console.log

--- src/taskMgmt/getTaskMap.js
+++ src/taskMgmt/getTaskMap.js
@@ -2,7 +2,6 @@ import { promises as fs } from 'fs';
 
 export const getTaskMap = async () => {
   const files = await fs.readdir('src/tasks');
-  console.log(files);
   // NOTE: restrict to .js files for now
   return files.filter((file) => file.endsWith('.js')).map((file) => file.substring(0, file.length - '.js'.length));
 };
